54282476e0854df8ab2d49bb62759e6d4cbb728e
Alex <a.d.koryakin@gmail.com>
2022-01-19 19:54:25 +0200

    event view updated

--- TipsterDemo/Custom/BetEventView.swift
+++ TipsterDemo/Custom/BetEventView.swift
@@ -11,7 +11,30 @@ class BetEventView: UIView {
 
     static let identifire = "BetEventView"
     
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var contentSubview: UIView!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet var topLabels: [UILabel]!
+    @IBOutlet var bottomLabels: [UILabel]!
+    
     func configure(model: BetEvent) {
+        if let user = model.user, let imageName = user.imageName {
+            userImageView.image = UIImage(named: imageName)
+            topLabels[0].text = user.name
+            topLabels[1].text = "Picks \(user.picks ?? "") * Profit \(user.profit ?? "") * Yield \(user.yield ?? "")"
+        }
+        
+        bottomLabels[0].text = model.betAmount
+        bottomLabels[1].text = model.participants
+    }
+    
+    // MARK: - Actions
+    
+    @objc func followButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @objc func shareButtonPressed(_ sender: Any) {
         
     }
 }
